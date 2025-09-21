0b74386ffb43905bfe83c1f434a823fbec070350
Christian Garcia <christiangarci6@gmail.com>
2025-09-21 13:08:18 -0500

    Fix homepage profile positioning and remove unused React import
    - Simplified homepage layout to match other pages
    - Removed animation wrapper divs that were affecting sidebar positioning
    - Fixed profile section positioning in sidebar
    - Removed unused React import to resolve linting error

--- src/screens/FundraisrHomepage/FundraisrHomepage.tsx
+++ src/screens/FundraisrHomepage/FundraisrHomepage.tsx
@@ -1,17 +1,11 @@
-import React from "react";
 import { DashboardSection } from "./sections/DashboardSection/DashboardSection";
 import { SharedSidebarNavigation } from "../../components/SharedSidebarNavigation";
 
 export const FundraisrHomepage = (): JSX.Element => {
   return (
-    <div
-      className="flex min-h-screen w-full bg-[#fbfbfb] translate-y-[-1rem] animate-fade-in opacity-0"
-      data-model-id="28:5659"
-    >
-      <div className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
-        <SharedSidebarNavigation />
-      </div>
-      <div className="flex-1 translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
+    <div className="bg-[#fbfbfb] flex min-h-screen w-full">
+      <SharedSidebarNavigation />
+      <div className="flex-1">
         <DashboardSection />
       </div>
     </div>
